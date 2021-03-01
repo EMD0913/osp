68c82583ec122cc89bf4f282ec6a4a1d4960b609
emd20200729 <emd_shop@yahoo.co.jp>
2021-03-01 20:08:26 +0900

    camplist.phpを表示できるところまで行ったが、文字化けしてしまう。

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,18 +5,17 @@ namespace App\Controller;
 use App\Controller\AppController;
 
 class AdminController extends AppController{
-    public $autoRender = false;
-    
+//     public $autoRender = false;
     public function index(){
         echo "<html><body><h1>Cakphp_prj</h1>";
         echo "<p>yoshida manabu</p></body></html>";
     }
-    
-    public function camplist() {
-        echo "HEllOOOOO";
-    }
-    
-    
-}
 
+    public function camplist() {
+//      4.0に対応したコード
+        $this->viewBuilder()->disableAutoLayout();
+        $text = "この文字列をViewに出力します。";
+        $this->set('text', $text);
+    }
+}
 ?>
